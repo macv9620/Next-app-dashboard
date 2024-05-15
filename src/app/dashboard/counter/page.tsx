e734f4c308a46dde3480522b5b8f0f6b3127e6a6
mateo_vasco <mateo.castano@sofka.com.co>
2024-05-14 19:46:29 -0500

    S4 Finished

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -7,13 +7,10 @@ export const metadata = {
 
 
 export default function CounterPage() {
-
-
   return (
     <div className="flex flex-col items-center justify-center w-full h-full">
       <span>Productos en el carrito</span>
       <CartCounter value={ 20 } />
-      
     </div>
   );
 }
